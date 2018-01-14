42146b1a282635a213bb8fd1de5ace7b45f7e073
jisung <jisung.smart@gmail.com>
2018-01-14 10:49:50 +0900

    Initial commit Meme2.0

---
+++ Meme2.0/MemeCollectionVC.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionVC.swift
+//  Meme2.0
+//
+//  Created by The book on 2018. 1. 14..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionVC: UICollectionView {
+    
+    
+}
---
+++ Meme2.0/MemeEditorVC.swift
@@ -0,0 +1,15 @@
+//
+//  MemeEditorVCd.swift
+//  Meme2.0
+//
+//  Created by The book on 2018. 1. 14..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeEditorVC {
+    
+    
+}
