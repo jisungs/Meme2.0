60667e2409c236500fbe24f8932d96196885d407
jisung <jisung.smart@gmail.com>
2018-01-17 07:31:21 +0900

    Add flowLayout IBOutlet in MemeCollectionView and add costant sapce and dimention in Viewdidload func

--- Meme2.0/MemeCollectionVC.swift
+++ Meme2.0/MemeCollectionVC.swift
@@ -15,10 +15,20 @@ class MemeCollectionVC: UICollectionViewController {
     
     var memes = [Meme]()
     
+    @IBOutlet weak var flowLayout:UICollectionViewFlowLayout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         memes = appDelegate.memes
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let space: CGFloat = 3.0
+        let dimension = (self.view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width:dimension, height:dimension)
+        
     }
 
     
