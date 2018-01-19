84f39458f1e716c4b100bece8d30e9b9dfc3a7bf
jisung <jisung.smart@gmail.com>
2018-01-20 07:38:02 +0900

    Add MemeDetailVC file Add outlet related MemeDetailVC file ongoing memeDetailVC

---
+++ Meme2.0/MemeDetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  MemeDetailVC.swift
+//  Meme2.0
+//
+//  Created by The book on 2018. 1. 20..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDetailVC : UIViewController {
+    
+    var memes = [Meme]()
+    
+    @IBOutlet weak var memeImageView: UIView!
+    @IBOutlet weak var memeLabel : UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+    
+}
